2ed469d4e60f09eca39d0cf4def00f0429b16286
Jisa627 <elochimu85@gmail.com>
2026-04-02 20:06:57 -0300

    Add files via upload

---
+++ about.md
@@ -0,0 +1,4 @@
+# Created with a non-actived version www.avs4you.com
+
+Inspired in "Unregistered HyperCam 2" mod by slideglide
+AVS Video Editor is a video editing software published by Online Media Technologies
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Geode/Geode.hpp>
+#include <Geode/ui/OverlayManager.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init(){
+		if (!MenuLayer::init()) {
+			return false;
+		}
+		auto winSize = CCDirector::get()->getWinSize();
+		auto scene = OverlayManager::get();
+
+		auto sprite = CCSprite::create("watermark.png"_spr);
+		sprite->setPosition({ winSize.width / 2, winSize.height / 2 });
+		scene->addChild(sprite, 9999);
+		sprite->retain();
+
+		return true;
+	}
+};
